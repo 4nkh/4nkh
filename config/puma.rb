e4c4108a9847691a62955ff9e5ee31b6c69aa4b1
Sp4rkplug <bbob784@gmail.com>
2024-02-09 11:43:19 -0500

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -28,6 +28,10 @@ if ENV["RAILS_ENV"] == "production"
   
   #state_path "#{shared_dir}/pids/puma.state"
   #activate_control_app
+  
+else
+  port ENV.fetch("PORT") { 3000 }
+  environment ENV.fetch("RAILS_ENV") { "development" }
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
@@ -35,10 +39,10 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+#port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+#environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }

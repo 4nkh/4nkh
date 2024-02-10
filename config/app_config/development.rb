9864bc528761204ba67d15a84539e7b60291e0d1
Sp4rkplug <bbob784@gmail.com>
2024-02-09 23:15:33 -0500

    fix

--- config/app_config/development.rb
+++ config/app_config/development.rb
@@ -10,13 +10,3 @@ AppConfig.routes = ApplicationConfiguration.new
 AppConfig.routes.subdomain = "www"
 AppConfig.routes.protocol = "http"
 AppConfig.routes.secure = false
-
-##config.hosts << /.*\.sc.net
-#config.hosts << www.sc.net:3000
-#Rails.application.config.hosts << /.*\.product\.com/
-
-# .env
-#Recaptcha.configure do |config|
-#  config.site_key  = '6Ld2GVgpAAAAAOzvczmIVDHJelL0LNSWdvXQ-PBD'
-#  config.secret_key = '6Ld2GVgpAAAAAIU-uzBAAqyapBJ2XkN7LYrs5l1y'
-#end
